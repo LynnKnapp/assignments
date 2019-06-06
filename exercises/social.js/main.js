0b3e09dc5e347dc9eb4abd05a26f5b38a8bc54ab
Lynn Knapp <ebonyeek@example.com>
2019-06-06 17:50:22 -0600

    finished travel-form

--- exercises/social.js/main.js
+++ exercises/social.js/main.js
@@ -3,37 +3,34 @@
 
 
 
-var ldsers = {
-    name: David,
+var ldsers = [
+    {
+    name: "David",
     age: 57,
     jack: "yes",
     rebellious: "yes",
-    interests: {  
-        gambling : [ "poker", "craps", "slots", "roulette"] ,
-        stuff: [ "drinking", "gaming", "coding"]},     
-        friends: {
-         name: "Don",
-         age: 55
-        },
-         interests: {
-             other: ["reading", "drinking"],
-             hiking: ["[Mount-Everest", "Canyon", "Pyrenees"]
-            },
-    name: John,
+    interests: [[ "poker", "craps", "slots", "roulette"],
+                [ "drinking", "gaming", "coding"]],     
+    friends: {
+           name: "Don",
+           age: 55,
+           sex: "male", 
+           interests: [["reading", "drinking"],
+                        ["[Mount-Everest", "Canyon", "Pyrenees"]]
+    },
+ }, {    
+    name: "John",
     age: 36,
     jack: "no",
     rebellious: "no",
-    interests: {
-        watchingMovies: ["Matrix", "Roadhouse", "John Quick" ],
-        other: [ "singing", "dancing"],
-    },
-    
-    
-}       
-        
-//                 friends: { 
-//                     name: "Jim",
-//                     age: "40",
-//                     interests: ["surfing", "skiing", "hiking"]
-// }
+    interests: [["Matrix", "Roadhouse", "John Quick" ],
+                [ "singing", "dancing"]],           
+    friends:{ 
+            name: "Jim",
+            age: "40",       
+            interests: ["surfing", "skiing", "hiking"]
+    }
+}]  
 
+console.log(ldsers[1].friends.interests[1])
+    
